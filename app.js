63261a13d4055aa3c877a29198e19f4306b8ae8c
Jackie Abert <jax.abert@gmail.com>
2020-08-11 17:10:11 -0400

    added code to the app file

--- app.js
+++ app.js
@@ -1,11 +1,42 @@
+require('dotenv').config()
+const cors = require('cors')
+const helmet = require('helmet');
 const express = require('express');
-const app = express();
 const movies = require('./movieList.js');
+const morgan = require('morgan');
+const app = express();
+app.use(morgan('dev'));
+app.use(cors());
+app.use(helmet());
+
+app.use(function validateBearerToken(req, res, next){
+    const bearerToken = req.get('Authorization').split(' ')[1]
+    const apiToken = process.env.API_TOKEN
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+        return res.status(401).json({ error: 'Unauthorized request' })
+    }
+    next()
+})
 
 app.get('/movie', (req, res) => {
     let response = movies;
     const { genre, country, avg_vote } = req.query;
-        res.json(response)
+     
+    if (genre) {
+        response = response.filter(movie => 
+            movie.genre.toLowerCase().includes(genre.toLowerCase()))
+    }
+    if (country) {
+        response = response.filter(movie => 
+            movie.country.toLowerCase().includes(country.toLowerCase()))
+    }
+    if (avg_vote) {
+        response = response.filter(movie => 
+            parseInt(movie.avg_vote) >= parseInt(avg_vote))
+    }
+    
+    
+    res.json(response)
 });
 
 app.listen(8080, () => {
