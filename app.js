f53ecbf4e23cae8e4c99261d5324e3616a2048fd
Jackie Abert <jax.abert@gmail.com>
2020-08-11 17:29:16 -0400

    fixed node module issue added git ignore file

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ app.use(cors());
 app.use(helmet());
 
 app.use(function validateBearerToken(req, res, next){
-    const bearerToken = req.get('Authorization').split(' ')[1]
     const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
     if (!authToken || authToken.split(' ')[1] !== apiToken) {
         return res.status(401).json({ error: 'Unauthorized request' })
     }
