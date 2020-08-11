e6ddfd94322967fec918e2758df229d4ae5a88d9
Queens Landing <queenslanding@queenss-mbp.attlocal.net>
2020-08-11 13:56:01 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const movies = require('./movie.js');
+
+app.get('/movie', (req, res) => {
+    const { genre, country, avg_vote } = req.query;
+        res.json(movies)
+});
+
+app.listen(8080, () => {
+    console.log('Server started on PORT 8080');
+});
